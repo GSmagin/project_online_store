837a39936c7dda08a62c15b55fd2303b4cecd328
Rocco <grog512new@mail.ru>
2024-03-20 02:50:25 +0300

    Реализация Задание 1

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from src.category import Category
 
 
 def main():
-    json_category_product_print()
+    # json_category_product_print()
 
     product1 = Product("Манго", "Египетское", 300, 20)
     product2 = Product("Мандарины", "Марроко", 150, 30)
-
+    product3 = Product("Бананы", "Эквадор", 159, 35)
     category1 = Category("Фрукты", "Фрукты импортные", [product1, product2])
-    print(category1.total_categories)
-    print(category1.total_unique_products)
-    print(product1.name)
-    print(product1.description)
-    print(product1.price)
-    print(product1.quantity)
+    # category1.product.append(product3)
+    category1.product = product3
+    print(category1.product)
+
 
 
 if __name__ == "__main__":
--- src/category.py
+++ src/category.py
@@ -1,12 +1,15 @@
+from src.product import Product
+
+
 class Category:
     """
-    total_categories всего категорий
+    :total_categories всего категорий
     :total_unique_products всего уникальные продукты
     """
     total_categories = 0
     total_unique_products = 0
 
-    def __init__(self, name: str, description: str, products: list):
+    def __init__(self, name: str, description: str, products):
         """
         :name название
         :description описание
@@ -14,6 +17,37 @@ class Category:
         """
         self.name = name
         self.description = description
-        self.products = products
-        Category.total_categories += 1
-        Category.total_unique_products += len(products)
+        self.__products = products
+        self.total_categories += 1
+        self.total_unique_products += len(products)
+
+
+    @property
+    def product(self):
+        product_list = []
+        for product in self.__products:
+            product_list.append({"name": product.name,
+                                 "description": product.description,
+                                 "price": product.price,
+                                 "quantity": product.quantity})
+
+
+        return product_list
+
+    @product.setter
+    def product(self, product: Product):
+        """
+        Добавляет продукт в категорию
+        :param product: экземпляр класса Product
+        """
+        self.__products.append(product)
+        self.total_unique_products += 1
+
+
+
+
+
+
+
+
+
